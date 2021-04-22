8c65ffbf38697575a9be86184c435a52034a4ccb
Yahya LAZREK <63449913+UUinc@users.noreply.github.com>
2021-04-22 22:09:20 +0000

    Created SetTimeToClass function and InitializeClassesSchedule function.

--- FreeTimerFinder.c
+++ FreeTimerFinder.c
@@ -14,15 +14,19 @@ struct Class
 };
 
 //Function Declaration
+void DisplayNames(int n,int Group);
+void DisplaySchedule(int n,int Group);
+void InitializeClassesSchedule();
+void SetTimeToClass(int n);
 
 //Global variables
-Class class[30];
-Class classGroup[17];
+Class class[25];
+Class classGroup[50];
 
 int main()
 {
-    int i=0, j=0, k=0, l=0;
-    char className[30][50] = {"A1","A2","A3","A4","A5","B1","B2","B3","B4","B5","C1","C2","C3","C4","C5","D1","D2","D3","D4","D5","CC1","CC2","CC3","CC4","CC5","LSCE","LTOPO","LRDM","LR","CMDS"};
+    int i=0, j=0, k=0, l=0, N;
+    char className[25][50] = {"A1","A2","A5","B2","B3","B4","B5","C2","C3","C4","C5","D2","D4","D5","CC1","CC2","CC3","CC4","CC5","LCSE","LAI","ST","LEPE","LTSE","LR"};
     FILE *file;
     char path[200],fileName[20]="/schedule.TXT",ch;
 
@@ -56,6 +60,7 @@ int main()
                 j++;
             }
         }
+        N = i+1;
         //Set time schedule
         i=j=k=l=0;
         while ((ch = fgetc(file)) != EOF)
@@ -82,22 +87,78 @@ int main()
     }
     fclose(file);
 
-    //Display All Names
-    for(i=0; i<30; i++)
-        printf("%s\n",class[i].name);
-    for(i=0; i<17; i++)
-        printf("%s\n",classGroup[i].name);
-    //Display All class group schedules
-    for(i=0; i<17; i++)
-    {
-        printf("\n----------Schedule : %s---------------\n",classGroup[i].name);
-        for(j=0; j<6; j++)
-        {
-            for(k=0; k<40; k++)
-                printf("%s ",classGroup[i].schedule[j][k]);
-            printf("\n");
-        }
-    }
+    InitializeClassesSchedule();
+
+    SetTimeToClass(N);
+
+    DisplayNames(N,0);
+    DisplayNames(N,1);   
+
+    DisplaySchedule(N,0);
+    DisplaySchedule(N,1);
+
     getch();
     return 0;
+}
+//display name (group = 0) show classes only (group != 0) show classes group
+void DisplayNames(int n,int Group)
+{
+    int i,j,k;
+
+    if(Group)
+        for(i=0; i<25; i++)
+            printf("%s\n",class[i].name);
+    else
+        for(i=0; i<n; i++)
+            printf("%s\n",classGroup[i].name);
+}
+void DisplaySchedule(int n,int Group)
+{
+    int i,j,k;
+
+    if(Group)
+        for(i=0; i<25; i++)
+        {
+            printf("\n----------Schedule : %s---------------\n",class[i].name);
+            for(j=0; j<6; j++)
+            {
+                for(k=0; k<40; k++)
+                    printf("%s ",class[i].schedule[j][k]);
+                printf("\n");
+            }
+        }
+    else
+        for(i=0; i<n; i++)
+        {
+            printf("\n----------Schedule : %s---------------\n",classGroup[i].name);
+            for(j=0; j<6; j++)
+            {
+                for(k=0; k<40; k++)
+                    printf("%s ",classGroup[i].schedule[j][k]);
+                printf("\n");
+            }
+        }
+}
+void InitializeClassesSchedule()
+{
+    int i,j,k;
+
+    for(i=0; i<25; i++)
+        for(j=0; j<6; j++)
+            for(k=0; k<40; k++)
+                strcpy(class[i].schedule[j][k],"0");
+}
+
+void SetTimeToClass(int n)
+{
+    int i,j,k,l;
+
+    for(i=0; i<n; i++)
+        for(j=0; j<6; j++)
+            for(k=0; k<40; k++)
+                for(l=0; l<25; l++)
+                    if(strcmp(classGroup[i].schedule[j][k],class[l].name) == 0)
+                    {
+                        strcpy(class[l].schedule[j][k],classGroup[i].name); break;
+                    }
 }
